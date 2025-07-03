0432864f4977856a57c9d038ebc27c60202a5255
mithun-karthickz <160773958+mithun-karthickz@users.noreply.github.com>
2025-07-03 21:30:53 +0530

    Update 1.2

--- adaptive_ecmp.py
+++ adaptive_ecmp.py
@@ -1,10 +1,9 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet
-
 
 class SimpleECMP(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -16,15 +15,13 @@ class SimpleECMP(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
-        parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
-        # Log switch connection
-        self.logger.info(f"Switch connected: DPID={datapath.id}")
-
-        # Install table-miss flow entry
+        # Install table-miss flow entry to send unknown packets to controller
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
@@ -33,15 +30,12 @@ class SimpleECMP(app_manager.RyuApp):
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         if buffer_id:
-            mod = parser.OFPFlowMod(
-                datapath=datapath, buffer_id=buffer_id,
-                priority=priority, match=match, instructions=inst
-            )
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
         else:
-            mod = parser.OFPFlowMod(
-                datapath=datapath, priority=priority,
-                match=match, instructions=inst
-            )
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -51,10 +45,14 @@ class SimpleECMP(app_manager.RyuApp):
         dpid = datapath.id
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth = pkt.get_protocol(ethernet.ethernet)
+        if eth.ethertype == 0x88cc:
+            return  # Ignore LLDP packets
+
         dst = eth.dst
         src = eth.src
 
@@ -68,14 +66,15 @@ class SimpleECMP(app_manager.RyuApp):
 
         actions = [parser.OFPActionOutput(out_port)]
 
-        # Log forwarding behavior
-        self.logger.info(f"Packet in DPID={dpid} SRC={src} DST={dst} IN={in_port} OUT={out_port}")
+        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
 
-        # Send packet out
-        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            return
+        else:
+            self.add_flow(datapath, 1, match, actions)
+
         out = parser.OFPPacketOut(
             datapath=datapath, buffer_id=msg.buffer_id,
-            in_port=in_port, actions=actions, data=data
-        )
+            in_port=in_port, actions=actions, data=msg.data)
         datapath.send_msg(out)
-
